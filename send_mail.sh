c467782943ac52f51526786154b5fc6598d488a1
Rohit Sarkar <rohitsarkar5398@gmail.com>
2021-05-09 00:39:40 +0530

    initial commit

---
+++ send_mail.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+{
+  echo "Subject: Found slots!"
+  echo "Slots available book now!" 
+} | ssmtp "rohitsarkar5398@gmail.com, satwaki.sarkar@gmail.com, suditibhowal14@gmail.com"
---
+++ vaccine.py
@@ -0,0 +1,56 @@
+import requests
+import json
+from threading import Timer
+import subprocess
+
+url = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict'
+
+DISTRICT_ID = '365'
+DATES = ['09-05-2021', '10-05-2021']
+params_1 = { 'date': '09-05-2021', 'district_id': '365' }
+params_2 = { 'date': '10-05-2021', 'district_id': '365' }
+
+with open('headers.json', 'r') as f:
+    headers = f.read()
+    headers = json.loads(headers)
+
+def send_email():
+    try:
+        subprocess.run(["./send_mail.sh"])
+    except:
+        print('Error sending email')
+
+def find_slots():
+    def check18(slot):
+        sessions = slot['sessions']
+        return any(map(lambda x: x['min_age_limit'] == 18 and x['available_capacity'] > 0, sessions))
+    try:
+        params = { "district_id": DISTRICT_ID }
+        slots = []
+        for date in DATES:
+           params["date"] = date
+           r = requests.get(url, params, headers=headers)
+           data = r.json()
+           data = data["centers"]
+           slots += list(filter(check18, data))
+
+        if len(slots) > 0:
+            print('Found slot')
+            send_email()
+        else:
+            print('No slot')
+    except Exception as e:
+        print(r.status_code)
+        print(e)
+
+def loop():
+    Timer(5.0, loop).start()
+    find_slots()
+
+loop()
+
+
+
+
+
+
